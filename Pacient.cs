08c677826c088005bb96a5174c3fe3375d252fdf
rikogrand <ilyakorondenko@mail.ru>
2023-03-19 09:35:37 +0300

    Добавьте файлы проекта.

---
+++ Pacient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    class Pacient
+    {
+        private string _fio;
+        private int _year;
+        private int _month;
+        private int _day;
+
+
+        public string FIO
+        {
+            get { return _fio; }
+            set
+            {
+                if (value.IndexOf(' ') > -1)
+                {
+
+                    _fio = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Введенные инициалы неккоректны");
+
+
+                }
+            }
+        }
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value >= 1980)
+                {
+                    _year = value;
+                }
+                else
+                {
+
+                    throw new ArgumentException("Год неккоректен");
+                }
+            }
+        }
+        public int Month
+        {
+            get { return _month; }
+            set
+            {
+                if (value <= 12)
+                {
+
+                    _month = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Месяц неккоректен");
+                }
+            }
+        }
+        public int Day
+        {
+            get { return _day; }
+            set
+            {
+                if (value <= 31)
+                {
+
+                    _day = value;
+                }
+                else
+                {
+                    throw new ArgumentException("День неккоректен");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var tmp = new DateTime(1, Month, 1);
+            return tmp.ToString("MMMM");
+        }
+        public int AgeResult(Pacient pacient)
+        {
+            int age = DateTime.Now.Year - pacient._year;
+            if (DateTime.Now.DayOfYear < new DateTime(pacient.Year, pacient.Month, pacient.Day).DayOfYear)
+                age--;
+            return age;
+        }
+        public string DateOfBirth(Pacient pacient)
+        {
+            string dateofbirth = pacient.Day.ToString() + "." + pacient.Month.ToString() + "." + pacient.Year.ToString();
+            return dateofbirth;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pr1
+{
+    class Program : Pacient
+    {
+        static void Main(string[] args)
+        {
+            string again = "yes";
+            while (again == "yes")
+            {
+                List<Pacient> pacients = new List<Pacient>();
+            var summerList = new List<string>();
+            var springList = new List<string>();
+            Pacient pacient1 = new Pacient();
+            pacient1.FIO = "Леонель Меси";
+            pacient1.Day = 18;
+            pacient1.Month = 8;
+            pacient1.Year = 2005;
+            pacients.Add(pacient1);
+
+
+            Pacient pacient2 = new Pacient();
+            pacient2.FIO = "Аафва Аваафв";
+            pacient2.Day = 10;
+            pacient2.Month = 4;
+            pacient2.Year = 2003;
+            pacients.Add(pacient2);
+
+            Console.WriteLine("Нажмите 1 если хотите просмотреть данные о " + pacient1.FIO + " или 2 если хотите посмотреть информацию о " + pacient2.FIO);
+            int choosepacients = Convert.ToInt32(Console.ReadLine());
+            if (!string.IsNullOrEmpty(pacient1.FIO) && choosepacients == 1)
+            {
+                Console.WriteLine("Инициалы пациента:\t " + pacient1.FIO);
+                Console.WriteLine("Месяц рождения:\t\t " + pacient1.ToString());
+                Console.WriteLine("Возраст: \t\t " + pacient1.AgeResult(pacient1));
+
+            }
+            else if (!string.IsNullOrEmpty(pacient2.FIO) && choosepacients == 2)
+            {
+                Console.WriteLine("Инициалы пациента:\t " + pacient2.FIO);
+                Console.WriteLine("Месяц рождения:\t\t " + pacient2.ToString());
+                Console.WriteLine("Возраст: \t\t " + pacient2.AgeResult(pacient2));
+
+            }
+
+            Console.WriteLine("Нажмите на кнопку 1 если нужно вывести список весенных дат рождения или 2 если нужно вывести летние");
+            int r1 = Convert.ToInt32(Console.ReadLine());
+            if (r1 == 1)
+            {
+                var spring = pacients.Where(s => s.Year >= 1990 & s.Year <= 2005 & s.Month >= 3 & s.Month <= 5).OrderBy(s => s);
+                foreach (Pacient p in spring)
+                {
+                    Console.WriteLine("Весенние даты рождения: " + p.DateOfBirth(p));
+
+                }
+            }
+            else if (r1 == 2)
+            {
+                var summer = pacients.Where(s => s.Month >= 6 & s.Month <= 8).OrderBy(s => s);
+                foreach (Pacient p2 in summer)
+                {
+
+                    Console.WriteLine("Летние даты рождения: " + p2.DateOfBirth(p2));
+
+                }
+            }
+                Console.WriteLine("Вы хотите продолжить работу с программой? (yes/no)");
+                again = Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
+
